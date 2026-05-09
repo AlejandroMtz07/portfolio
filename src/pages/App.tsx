e6f24fe3d78cd7fa084f0edc8e9285a796a0ff33
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-09 13:33:42 -0700

    First commit.

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+    return (
+        <div className="min-h-screen bg-[#6EB1FF]">
+            <img src="../assets/clouds_background.png" alt="Clouds" className="contrast-125"/>
+        </div>
+    )
+}
+
+export default App
