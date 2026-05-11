d876c4e1e3a6d8dc60d293b540a7bdf812c21df6
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-11 00:53:49 -0700

    Initial parts for the project card.

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,25 @@
+
+type ProjectCardProps = {
+    project_title: string,
+    project_description: string,
+    project_duration: number,
+    project_tools: string[]
+}
+
+export default function ProjectCard(props: ProjectCardProps) {
+    return (
+        <div className="bg-[url('../assets/button_bg_image.png')] 
+          border-t-[#AAA] border-l-[#AAA] border-b-[#565656] border-r-[#565656] border-4  
+          m-5 lg:w-50 lg:h-80 text-white "
+        >
+            <h2>{props.project_title}</h2>
+            <div className="grid grid-cols-2">
+                <h3>Development tools used: <br />{props.project_tools}</h3>
+                <h3>Duration: {props.project_duration} months</h3>
+            </div>
+            <h3 className="mt-5">
+                {props.project_description}
+            </h3>
+        </div>
+    )
+}
--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,13 +1,14 @@
+import ProjectCard from "../components/ProjectCard"
 
 export default function Projects() {
-  return (
-    <div className="grid grid-cols-2 text-center mt-32">
-        <div>
-          MichiCan (JSP y MySQL)
-        </div>
-        <div>
-          Task Manager (API y Frontend)
-        </div>
-    </div>
-  )
+	return (
+		<div className="lg:grid grid-cols-2 text-center mt-32 font-mc text-2xl">
+			<ProjectCard
+				project_title="MichiCan"
+				project_tools={["Java, JSP, SQLite"]}
+				project_duration={6}
+				project_description="System designed for the vet business management"
+			/>
+		</div>
+	)
 }
