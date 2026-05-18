69fcd59fa119c30d7945176aec43a66e0cf26b4c
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-17 17:16:00 -0700

    Changing styles of the 404 not found page.

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,8 +1,13 @@
 
 export default function NotFound() {
   return (
-    <div className="h-screen flex items-center justify-center">
-        Page not found
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <div>  
+        <img src="../assets/icons/unplugged_icon.png" alt="Unplugged icon" className="h-40"/>
+        <h2 className="font-mc text-3xl text-white">
+          Page not found
+        </h2>
+      </div>
     </div>
   )
 }
